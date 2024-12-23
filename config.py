95febdbe27cfe0c475cff86171dd9f8dbe963145
BapuGYT <126301222+BapuGYT@users.noreply.github.com>
2024-12-23 07:24:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-
 import re
 import os
 from os import getenv, environ
@@ -22,20 +21,20 @@ def is_enabled(value, default):
 
       
 # Owner Information
-API_ID = int(environ.get("API_ID", "29640476"))
-API_HASH = environ.get("API_HASH", "824d5b93ac8c8fddcf4088a67ecd5222")
-ADMINS = int(environ.get("ADMINS", "6138367360"))
+API_ID = int(environ.get("API_ID", "26551135"))
+API_HASH = environ.get("API_HASH", "19dacd8bfa909cb8abd1c0338ca84ad0")
+ADMINS = int(environ.get("ADMINS", "7267123227"))
 
 # Database Information
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://infotechhero890:7c2qvHdJUYqTOaMa@cluster0.veojhex.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-CDB_NAME = environ.get("CDB_NAME", "clonevjbotz")
-DB_URI = environ.get("DB_URI", "mongodb+srv://thahero196:lP9Fb6aKL7T0y47U@cluster0.whs2bkj.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = environ.get("DB_NAME", "vjbotz")
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://prolitebapug:hl1MBlsndjhMSGBL@hakur.enyjhyz.mongodb.net/?retryWrites=true&w=majority&appName=Hakur")
+CDB_NAME = environ.get("CDB_NAME", "Hakur")
+DB_URI = environ.get("DB_URI", "mongodb+srv://vkrm:UuZUTL7rbto6RGy2@vkrm.x4j95.mongodb.net/?retryWrites=true&w=majority&appName=vkrm")
+DB_NAME = environ.get("DB_NAME", "vkrm")
 
 
 # Bot Information
-BOT_TOKEN = environ.get("BOT_TOKEN", "7097361555:AAHOhbFWGflLqJQcY5vWleTU-iXvfai_rXs")
-BOT_USERNAME = environ.get("BOT_USERNAME", "Anime_file_store1_bot") # your bot username without @
+BOT_TOKEN = environ.get("BOT_TOKEN", "7499167063:AAEsbaP2eBDk3DYq9zzdk1kzfTdPPAMt_kA")
+BOT_USERNAME = environ.get("BOT_USERNAME", "Doraemon_files_bot") # your bot username without @
 PICS = (environ.get('PICS', 'https://te.legra.ph/file/66604e5911df5a0c43ffa.jpg https://graph.org/file/f5a26ac21b060d5787ea3.jpg https://graph.org/file/30539d8f1fc92423d6a19.jpg https://graph.org/file/701ff72fb43d85e073f03.jpg')).split() # Bot Start Picture
 
 # Auto Delete Information
@@ -43,11 +42,11 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002005121747"))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1002067183105')).split()]
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002204209889"))
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1002088061586')).split()]
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Don't Remove Credit Tg - @Vikram_Singh_V12
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@BapuGGaming
 # Ask Doubt on telegram @KingVJ01
 
 # File Caption Information
@@ -63,10 +62,10 @@ PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 # File Stream Config
 class Var(object):
     MULTI_CLIENT = True
-    name = str(getenv('name', 'filetolinkvjbot'))
+    name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002124914386'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002088061586)
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
